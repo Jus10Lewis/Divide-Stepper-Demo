f04b3ae05253d605b83dcfc5d0fc2bf91bae21e1
Justin Lewis <robodad@Jus10MacBook.local>
2018-09-24 20:36:13 -0500

    Created and hooked up UI

---
+++ DivideStepperDemo/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  DivideStepperDemo
+//
+//  Created by Justin Lewis on 9/24/18.
+//  Copyright © 2018 Justin Lewis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var num1TextField: UITextField!
+    @IBOutlet weak var num2Label: UILabel!
+    @IBOutlet weak var answerLabel1: UILabel!
+    @IBOutlet weak var answerLabel2: UILabel!
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        
+        
+    }
+    
+    
+    @IBAction func divideNumbers(_ sender: UIButton) {
+        
+    }
+    
+
+}
+
